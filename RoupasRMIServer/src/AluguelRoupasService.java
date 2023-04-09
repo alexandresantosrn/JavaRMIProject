879201ad41672d491b921b611c0270ec69ea5777
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-04-09 13:16:43 -0300

    Implementando servidor + interface básica

--- RoupasRMIServer/src/AluguelRoupasService.java
+++ RoupasRMIServer/src/AluguelRoupasService.java
@@ -5,5 +5,4 @@ import java.util.Date;
 public interface AluguelRoupasService extends Remote {
 
 	String findRoupasDisponiveis(String estilo, Date inicioAluguel, Date fimAluguel) throws RemoteException;
-
 }
