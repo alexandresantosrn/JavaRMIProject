9c5a5f46091fba3f2b2a68274ee82f07b398edf1
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-04-09 13:33:17 -0300

    Implementando Classe Roupa

---
+++ RoupasRMIServer/src/Roupa.java
@@ -0,0 +1,32 @@
+
+public class Roupa {
+
+	String nome;
+	String estilo;
+	float preco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(String estilo) {
+		this.estilo = estilo;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+}
