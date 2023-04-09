b093bed38ebfece3880e13b806e8cae581037076
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-04-09 13:07:46 -0300

    Criando interface cliente

---
+++ RoupasRMIClient/src/AluguelRoupasService.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+public interface AluguelRoupasService extends Remote {
+	
+	String findRoupasDisponiveis(String estilo, Date inicioAluguel, Date fimAluguel) throws RemoteException;
+
+}
