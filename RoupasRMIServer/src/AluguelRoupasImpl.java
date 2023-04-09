22721bcaa8dfb6ca5a23f415f28dbc8050b6512c
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-04-09 14:23:22 -0300

    Implementando classe de alugueis

--- RoupasRMIServer/src/AluguelRoupasImpl.java
+++ RoupasRMIServer/src/AluguelRoupasImpl.java
@@ -1,4 +1,20 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Date;
 
-public class AluguelRoupasImpl {
+public class AluguelRoupasImpl extends UnicastRemoteObject implements AluguelRoupasService{
 
+	protected AluguelRoupasImpl() throws RemoteException {
+		super();		
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String findRoupasDisponiveis(String estilo, Date inicioAluguel, Date fimAluguel) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
