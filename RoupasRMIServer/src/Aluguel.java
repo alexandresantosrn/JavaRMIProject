f50606cd80678526138527eb19f1ca665afbca9f
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-04-10 21:34:10 -0300

    Criando classe de aluguel

---
+++ RoupasRMIServer/src/Aluguel.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public class Aluguel {
+
+	Roupa roupa;
+	Date inicio;
+	Date fim;
+
+	public Roupa getRoupa() {
+		return roupa;
+	}
+
+	public void setRoupa(Roupa roupa) {
+		this.roupa = roupa;
+	}
+
+	public Date getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+
+	public Date getFim() {
+		return fim;
+	}
+
+	public void setFim(Date fim) {
+		this.fim = fim;
+	}
+
+}
