fb111740a4f7ec96c65291578822e93090b651f1
unknown <alexandre.santos@imd.local>
2023-04-19 13:31:53 -0300

    Corrigindo chamada da IP

--- RoupasRMIClient/src/Main.java
+++ RoupasRMIClient/src/Main.java
@@ -10,8 +10,8 @@ public class Main {
 
 	public static void main(String[] args) throws RemoteException, NotBoundException {
 
-		// Registry registry = LocateRegistry.getRegistry("localhost");
-		Registry registry = LocateRegistry.getRegistry("192.168.2.115");
+		Registry registry = LocateRegistry.getRegistry("localhost");
+		//Registry registry = LocateRegistry.getRegistry("192.168.2.115");
 		AluguelRoupasService client = (AluguelRoupasService) registry.lookup("Roupas");
 		LocalDate start, end;
 		
